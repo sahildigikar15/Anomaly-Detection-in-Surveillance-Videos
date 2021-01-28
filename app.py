a7f4e95777e57b8ba4d8f7e10978d2bcbfc992a5
Sahil <sahildigikar15@gmail.com>
2021-01-28 15:11:48 +0530

    validation login/reg

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request,redirect,session,url_for,g
 import os
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -9,10 +10,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/final_year_proj
 db = SQLAlchemy(app)
 class User(db.Model):
     # sr,name,email,password
-    sr = db.Column(db.Integer, primary_key=True)
+    sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50),nullable=False)
     email = db.Column(db.String(50), nullable=False)
+    number = db.Column(db.String(12), nullable=False)
     password = db.Column(db.String(15), nullable=False)
+    date = db.Column(db.String(12), nullable= True)
 
 @app.route("/",methods = ['GET','POST'])
 def home():
@@ -29,4 +32,37 @@ def login():
 @app.route("/register")
 def register():
     return render_template('register.html')
+
+@app.route("/add_user",methods=['POST'])
+def add_user():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    number = request.form.get('number')
+    password = request.form.get('password')
+    re_password = request.form.get('re_password')
+    entry = User(name=name,number=number,email=email,date = datetime.now(),password=password)
+
+    if password == re_password:
+
+        db.session.add(entry)
+        db.session.commit()
+        return redirect('/')
+    else:
+        msg = "Password mismatch"
+        return render_template('register.html',msg=msg)
+
+@app.route("/login_validation",methods = ['POST'])
+def login_validation():
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user_data = User.query.filter_by(email=email, password=password).first()
+    try:
+        if user_data.email is not None and user_data is not None:
+            return redirect('/')
+    except:
+        msg = "Incorrect username/password!"
+        return render_template('login.html',msg=msg)
+
 app.run(port=8181,debug=True)
